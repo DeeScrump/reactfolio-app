ebd872221bc1a247426460752af5082e803081ad
D Kornbluth <dwight.kornbluth@gmail.com>
2021-11-17 09:34:41 -0500

    updated contact me section with forms

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,48 @@ import NavBar from './Navigation';
 import FootBar from './Footer';
 import Project from './Project';
 import '../styles/styles.css';
+import { validateEmail } from '../utils/helpers';
 
 
 export default function Header() {
   const [currentPage, setCurrentPage] = useState('About');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [textArea, setTextArea] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+      setTextArea(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !name) {
+      setErrorMessage('Email or Name is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setName('');
+    setTextArea('');
+    setEmail('');
+  };
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
@@ -36,7 +74,38 @@ export default function Header() {
         return (
             <div>
                 <h1>Contact Me</h1>
-                <p>DID THIS SECTION POPULATE</p>
+                <form className="form">
+                    <input
+                    value={name}
+                    name="Name:"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="username"
+                    />
+                    <br></br>
+                    <input
+                    value={email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type="email"
+                    placeholder="email"
+                    />
+                    <br></br>
+                    <textarea
+                    value={textArea}
+                    name="text"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder=""
+                    />
+                    <br></br>
+                    <button type="button" onClick={handleFormSubmit}>Submit</button>
+                </form>
+                {errorMessage && (
+                    <div>
+                    <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <FootBar />
             </div>
         );
@@ -44,7 +113,7 @@ export default function Header() {
     return (
         <div>
             <h1>Resume</h1>
-            <p>Click here for my resume.</p>
+            <p>Input my Resume Here</p>
             <FootBar />
         </div>
       );
