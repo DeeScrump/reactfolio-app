9dfd2565a5acac55f647ee0a15af4df5d5b30da5
D Kornbluth <dwight.kornbluth@gmail.com>
2021-11-16 21:58:15 -0500

    added footer

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function footBar() {
+    return (
+        <footer class="card-body text-center">
+            <div class="card-body d-wrap-flex">
+                <a class="px-2" href="#352-871-4481" alt="Mobile Number">M: 352-871-4481</a>
+                <a class="px-2" href="#email" alt="Email">dwight.kornbluth@gmail.com</a>
+                <a class="px-2" href="https://github.com/DeeScrump" alt="Link to Github Repositories">Github</a>
+                <a class="px-2" href="https://my.indeed.com/p/dwightk-zuz0jhm" alt="Link to Indeed">Indeed</a>
+                <a class="px-2" href="https://www.linkedin.com/in/dwight-c-kornbluth-ii/" alt="Link to LinkedIn">LinkedIn</a>
+                <a class="px-2" href="https://www.instagram.com/flga.agent.lifehealth.realtor/" alt="Link to Instagram">Instagram</a>
+            </div>
+    </footer>
+    )
+};
